f5a794ac2a771d7922c88565c4581d1a5731319d
phil <phil@emiphil.com>
2021-07-13 16:42:16 -0600

    start work on clox

--- dbllist.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by phil on 2021-05-03.
-//
-
-#ifndef CLOX_DBLLIST_H
-#define CLOX_DBLLIST_H
-
-#endif //CLOX_DBLLIST_H
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by phil on 2021-05-03.
+//
+
+#ifndef CLOX_MAIN_H
+#define CLOX_MAIN_H
+
+#endif //CLOX_MAIN_H
