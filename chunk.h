675b358aedb2a7dcfb9853a42a5b42a96f8bca73
phil <phil@emiphil.com>
2021-07-19 17:21:05 -0600

    ch 15

---
+++ chunk.h
@@ -0,0 +1,31 @@
+#ifndef CLOX_CHUNK_H
+#define CLOX_CHUNK_H
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+    OP_CONSTANT,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+    OP_NEGATE,
+    OP_RETURN,
+} OpCode;
+
+// Dynamic Array for storing the bytecode
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t *code;
+    int* lines;
+    ValueArray constants;
+} Chunk;
+
+void init_chunk(Chunk *chunk);
+void free_chunk(Chunk *chunk);
+void write_chunk(Chunk *chunk, uint8_t byte, int line);
+int add_constant(Chunk* chunk, Value value);
+
+#endif //CLOX_CHUNK_H
--- main.h
+++ main.h
@@ -1,7 +1,3 @@
-//
-// Created by phil on 2021-05-03.
-//
-
 #ifndef CLOX_MAIN_H
 #define CLOX_MAIN_H
 
