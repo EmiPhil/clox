5fdcfbbd23693359a3fbac62e015ce646468b506
phil <phil@emiphil.com>
2021-07-20 17:45:01 -0600

    ch 17

--- common.h
+++ common.h
@@ -1,10 +1,11 @@
 #ifndef CLOX_COMMON_H
 #define CLOX_COMMON_H
 
-# include <stdbool.h>
-# include <stddef.h>
-# include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
-# define DEBUG_TRACE_EXECUTION
+#define DEBUG_PRINT_CODE
+#define DEBUG_TRACE_EXECUTION
 
 #endif //CLOX_COMMON_H
