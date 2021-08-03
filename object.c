8364617989e88333bdd20107418805342b96889f
phil <phil@emiphil.com>
2021-08-03 17:40:59 -0600

    ch 20

--- object.c
+++ object.c
@@ -3,6 +3,7 @@
 
 #include "memory.h"
 #include "object.h"
+#include "table.h"
 #include "value.h"
 #include "vm.h"
 
@@ -14,29 +15,51 @@ static Obj * allocate_object(size_t size, ObjType type) {
     object->type = type;
 
     // we keep track of object allocations directly in the objects which allows the vm to traverse the linked list of
-    // object allocations, even if they reference to them has otherwise been discarded
+    // object allocations, even if the reference to them has otherwise been discarded
     object->next = vm.objects;
     vm.objects = object;
 
     return object;
 }
 
-static ObjString * allocate_string(char * chars, int length) {
-    ObjString * string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+static ObjString * allocate_string(char *chars, int length, uint32_t hash) {
+    ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
     string->length = length;
     string->chars = chars;
+    string->hash = hash;
+    table_set(&vm.strings, string, NIL_VAL);
     return string;
 }
 
-ObjString * take_string(char * chars, int length) {
-    return allocate_string(chars, length);
+static uint32_t hash_string(const char *key, int length) {
+    uint32_t hash = 2166136261u;
+    for (int i = 0; i < length; i++) {
+        hash ^= (uint8_t)key[i];
+        hash *= 16777619;
+    }
+    return hash;
 }
 
-ObjString * copy_string(const char * chars, int length) {
+ObjString * take_string(char *chars, int length) {
+    uint32_t hash = hash_string(chars, length);
+    ObjString *interned = table_find_string(&vm.strings, chars, length, hash);
+    if (interned != NULL) {
+        FREE_ARRAY(char, chars, length + 1);
+        return interned;
+    }
+
+    return allocate_string(chars, length, hash);
+}
+
+ObjString * copy_string(const char *chars, int length) {
+    uint32_t hash = hash_string(chars, length);
+    ObjString *interned = table_find_string(&vm.strings, chars, length, hash);
+    if (interned != NULL) return interned;
+
     char * heap_chars = ALLOCATE(char, length + 1);
     memcpy(heap_chars, chars, length);
     heap_chars[length] = '\0';
-    return allocate_string(heap_chars, length);
+    return allocate_string(heap_chars, length, hash);
 }
 
 void print_object(Value value) {
